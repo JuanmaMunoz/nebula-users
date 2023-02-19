45df3ec08fcb5a9194c206d2068b54a180005526
Jua Manuel Muñoz <juan.munoz@vintegris.com>
2023-02-19 10:41:42 +0100

    redirect cert

--- src/app/shared-module/components/not-found/not-found.component.ts
+++ src/app/shared-module/components/not-found/not-found.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { SessionService } from '../../services/session.service';
 
 @Component({
@@ -7,13 +8,20 @@ import { SessionService } from '../../services/session.service';
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent implements OnInit, OnDestroy {
+  private subscription = new Subscription();
   constructor(private router: Router, private sessionService: SessionService) {}
   ngOnInit(): void {
-    this.sessionService.checkSession().subscribe((session: boolean) => {
-      if (!session) {
-        this.router.navigate(['/login/auth']);
-      }
-    });
+    this.subscription.add(
+      this.sessionService.checkSession().subscribe((session: boolean) => {
+        if (!session) {
+          this.router.navigate(['/login/auth']);
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
